866c9eb30e27fe8894651eb4f60f08096756011c
Tim Bueno <timbueno@gmail.com>
2015-08-16 18:45:48 -0400

    Split out cheapjack manager and file. Added Failed state. Completed file handling.

---
+++ Cheapjack/CheapjackFile.swift
@@ -0,0 +1,187 @@
+//
+//  CheapjackFile.swift
+//  TBDropbox
+//
+//  Created by Tim Bueno on 2015-08-04.
+//  Copyright © 2015 Tim Bueno. All rights reserved.
+//
+
+import Foundation
+
+
+public protocol CheapjackFileDelegate: class {
+    func cheapjackFile(file: CheapjackFile, didChangeState from: CheapjackFile.State, to: CheapjackFile.State)
+    func cheapjackFile(file: CheapjackFile, didUpdateProgress progress: Double, totalBytesWritten: Int64, totalBytesExpectedToWrite: Int64)
+}
+
+
+public func ==(lhs: CheapjackFile, rhs: CheapjackFile) -> Bool {
+    return lhs.identifier == rhs.identifier
+}
+
+
+extension CheapjackFile.State: Equatable {
+}
+
+public func ==(lhs: CheapjackFile.State, rhs: CheapjackFile.State) -> Bool {
+    switch (lhs, rhs) {
+    case (let .Paused(data1), let .Paused(data2)):
+        return data1 == data2
+    case (.Unknown, .Unknown):
+        return true
+    case (.Waiting, .Waiting):
+        return true
+    case (.Downloading, .Downloading):
+        return true
+    case (.Finished, .Finished):
+        return true
+    case (.Cancelled, .Cancelled):
+        return true
+    case (.Failed, .Failed):
+        return true
+    default:
+        return false
+    }
+}
+
+
+public class CheapjackFile: Equatable {
+    
+    // A listener may implement either of delegate and blocks.
+    public class Listener {
+        
+        public typealias DidChangeStateBlock = (from: CheapjackFile.State, to: CheapjackFile.State) -> (Void)
+        public typealias DidUpdateProgressBlock = (progress: Double, totalBytesWritten: Int64, totalBytesExpectedToWrite: Int64) -> (Void)
+        
+        
+        public weak var delegate: CheapjackFileDelegate?
+        public var didChangeStateBlock: CheapjackFile.Listener.DidChangeStateBlock?
+        public var didUpdateProgressBlock: CheapjackFile.Listener.DidUpdateProgressBlock?
+        
+        public init(delegate: CheapjackFileDelegate? = nil, didChangeStateBlock: CheapjackFile.Listener.DidChangeStateBlock? = nil, didUpdateProgressBlock: CheapjackFile.Listener.DidUpdateProgressBlock? = nil) {
+            self.delegate = delegate
+            self.didChangeStateBlock = didChangeStateBlock
+            self.didUpdateProgressBlock = didUpdateProgressBlock
+        }
+        
+    }
+    
+    
+    // File states default to .Unknown
+    public enum State {
+        case Unknown
+        case Waiting
+        case Downloading
+        case Paused(NSData)
+        case Finished
+        case Cancelled
+        case Failed
+    }
+    
+    
+    public typealias Identifier = String
+    
+    
+    // MARK: - CheapjackFile public properties
+    
+    internal weak var manager: CheapjackManager?
+    public var identifier: CheapjackFile.Identifier
+    public var url: NSURL
+    public var progress: Double {
+        if totalBytesExpectedToWrite > 0 {
+            return Double(totalBytesWritten) / Double(totalBytesExpectedToWrite)
+        } else {
+            return 0.0
+        }
+    }
+    
+    // MARK: - CheapjackFile public read-only properties
+    
+    public private(set) var lastState: CheapjackFile.State
+    public private(set) var state: CheapjackFile.State {
+        willSet {
+            lastState = state
+        }
+        didSet {
+            notifyChangeStateListeners()
+        }
+    }
+    public private(set) var totalBytesExpectedToWrite: Int64
+    public private(set) var totalBytesWritten: Int64 {
+        didSet {
+            notifyUpdateProgressListeners()
+        }
+    }
+    
+    // MARK: - CheapjackFile private properties
+    
+    internal var listeners: [CheapjackFile.Listener]
+    internal var downloadTask: NSURLSessionDownloadTask?
+    
+    public init(identifier: CheapjackFile.Identifier, url: NSURL, listeners: [CheapjackFile.Listener]? = nil) {
+        self.identifier = identifier
+        self.url = url
+        self.state = .Unknown
+        self.lastState = .Unknown
+        self.totalBytesWritten = 0
+        self.totalBytesExpectedToWrite = 0
+        self.listeners = listeners ?? Array<CheapjackFile.Listener>()
+    }
+    
+    // MARK: - CheapjackFile private setter methods
+    
+    private func addListener(listener: CheapjackFile.Listener) {
+        listeners.append(listener)
+    }
+    
+    internal func setState(to: CheapjackFile.State) {
+        state = to
+    }
+    
+    internal func setTotalBytesWritten(bytes: Int64) {
+        totalBytesWritten = bytes
+    }
+    
+    internal func setTotalBytesExpectedToWrite(bytes: Int64) {
+        totalBytesExpectedToWrite = bytes
+    }
+    
+    // MARK: - CheapjackFile private notify methods
+    
+    private func notifyChangeStateListeners() {
+        if let manager = manager {
+            // CheapjackDelegate
+            manager.delegate?.cheapjackManager(manager, didChangeState: lastState, to: state, forFile: self)
+        }
+        
+        // CheapjackFile.Listener
+        for listener in listeners {
+            // CheapjackFileDelegate
+            listener.delegate?.cheapjackFile(self, didChangeState: lastState, to: state)
+            
+            // CheapjackFile.Listener.DidChangeStateBlock
+            if let didChangeStateBlock = listener.didChangeStateBlock {
+                didChangeStateBlock(from: lastState, to: state)
+            }
+        }
+    }
+    
+    private func notifyUpdateProgressListeners() {
+        if let manager = manager {
+            // CheapjackDelegate
+            manager.delegate?.cheapjackManager(manager, didUpdateProgress: progress, totalBytesWritten: totalBytesWritten, totalBytesExpectedToWrite: totalBytesExpectedToWrite, forFile: self)
+        }
+        
+        // CheapjackFile.Listener
+        for listener in listeners {
+            // CheapjackFileDelegate
+            listener.delegate?.cheapjackFile(self, didUpdateProgress: progress, totalBytesWritten: totalBytesWritten, totalBytesExpectedToWrite: totalBytesExpectedToWrite)
+            
+            // CheapjackFile.Listener.DidUpdateProgressBlock
+            if let didUpdateProgressBlock = listener.didUpdateProgressBlock {
+                didUpdateProgressBlock(progress: progress, totalBytesWritten: totalBytesWritten, totalBytesExpectedToWrite: totalBytesExpectedToWrite)
+            }
+        }
+    }
+    
+}
---
+++ Cheapjack/CheapjackManager.swift
@@ -0,0 +1,233 @@
+//
+//  CheapjackManager.swift
+//  TBDropbox
+//
+//  Created by Tim Bueno on 2015-08-04.
+//  Copyright © 2015 Tim Bueno. All rights reserved.
+//
+
+import Foundation
+
+
+public protocol CheapjackDelegate: class {
+    func cheapjackManager(manager: CheapjackManager, didChangeState from: CheapjackFile.State, to: CheapjackFile.State, forFile file: CheapjackFile)
+    func cheapjackManager(manager: CheapjackManager, didUpdateProgress progress: Double, totalBytesWritten: Int64, totalBytesExpectedToWrite: Int64, forFile file: CheapjackFile)
+    func cheapjackManager(manager: CheapjackManager, didReceiveError error: NSError?)
+    func cheapjackManager(manager: CheapjackManager, didFinishDownloading withSession: NSURLSession, downloadTask: NSURLSessionDownloadTask, url: NSURL, forFile file: CheapjackFile)
+}
+
+
+struct SessionProperties {
+    static let backgroundSessionIdentifier = "com.gurpartap.Cheapjack"
+}
+
+
+public class CheapjackManager: NSObject {
+    
+    public typealias DidFinishDownloadingBlock = (session: NSURLSession, downloadTask: NSURLSessionDownloadTask, url: NSURL, file: CheapjackFile) -> (Void)
+    
+    public weak var delegate: CheapjackDelegate?
+    public var didFinishDownloadingBlock: CheapjackManager.DidFinishDownloadingBlock?
+    
+    public var files: Dictionary<CheapjackFile.Identifier, CheapjackFile>
+    var backgroundSession: NSURLSession!
+    
+    public static let sharedManager = CheapjackManager()
+    
+    
+    override init() {
+        files = Dictionary<CheapjackFile.Identifier, CheapjackFile>()
+        
+        super.init()
+        
+        let backgroundSessionConfiguration = NSURLSessionConfiguration.backgroundSessionConfigurationWithIdentifier(SessionProperties.backgroundSessionIdentifier)
+        backgroundSession = NSURLSession(configuration: backgroundSessionConfiguration, delegate: self, delegateQueue: nil)
+    }
+    
+    // Helper method for starting a download for a new CheapjackFile instance.
+    public func download(url: NSURL, identifier: CheapjackFile.Identifier, delegate: CheapjackFileDelegate? = nil, didChangeStateBlock: CheapjackFile.Listener.DidChangeStateBlock? = nil, didUpdateProgressBlock: CheapjackFile.Listener.DidUpdateProgressBlock? = nil) {
+        let listener = CheapjackFile.Listener(delegate: delegate, didChangeStateBlock: didChangeStateBlock, didUpdateProgressBlock: didUpdateProgressBlock)
+        let file = CheapjackFile(identifier: identifier, url: url, listeners: [listener])
+        resume(file)
+    }
+    
+    public func pendingDownloads() -> Int {
+        return self.files.filter({ (identifier, file) in
+            return file.state != .Finished && file.state != .Cancelled
+        }).count
+    }
+    
+}
+
+
+// MARK: - Action on file with identifier
+
+extension CheapjackManager {
+    
+    public func resume(identifier: CheapjackFile.Identifier) -> Bool {
+        if let file = files[identifier] {
+            resume(file)
+            return true
+        }
+        return false
+    }
+    
+    public func pause(identifier: CheapjackFile.Identifier) -> Bool {
+        if let file = files[identifier] {
+            pause(file)
+            return true
+        }
+        return false
+    }
+    
+    public func cancel(identifier: CheapjackFile.Identifier) -> Bool {
+        if let file = files[identifier] {
+            cancel(file)
+            return true
+        }
+        return false
+    }
+    
+}
+
+
+// MARK: - Action on CheapjackFile
+
+extension CheapjackManager {
+    
+    public func resume(file: CheapjackFile) {
+        file.manager = self
+        files[file.identifier] = file
+        
+        
+        switch file.state {
+        case .Paused(let data):
+            file.setState(.Waiting)
+            file.downloadTask = backgroundSession.downloadTaskWithResumeData(data)
+        default:
+            file.setState(.Waiting)
+            file.downloadTask = backgroundSession.downloadTaskWithURL(file.url)
+        }
+        file.downloadTask?.taskDescription = file.identifier
+        file.downloadTask?.resume()
+    }
+    
+    public func pause(file: CheapjackFile) {
+        file.downloadTask?.cancelByProducingResumeData({ resumeDataOrNil in
+            if let data = resumeDataOrNil {
+                file.setState(.Paused(data))
+                print("paused")
+            } else {
+                file.setState(.Cancelled)
+                // TODO: Handle server not supporting resumes.
+                print("can't resume this later. cancelling instead.")
+            }
+        })
+    }
+    
+    public func cancel(file: CheapjackFile) {
+        file.setState(.Cancelled)
+        file.downloadTask?.cancel()
+    }
+    
+}
+
+
+// MARK: - Action on all
+
+extension CheapjackManager {
+    
+    public func resumeAll() {
+        for file in files.values {
+            resume(file)
+        }
+    }
+    
+    public func pauseAll() {
+        for file in files.values {
+            pause(file)
+        }
+    }
+    
+    public func cancelAll() {
+        for file in files.values {
+            cancel(file)
+        }
+    }
+    
+}
+
+
+extension CheapjackManager {
+    public func remove(identifier: CheapjackFile.Identifier) {
+        files.removeValueForKey(identifier)
+    }
+    
+    public func remove(filesWithState: CheapjackFile.State) {
+        var filesCopy = files
+        for (identifier, file) in filesCopy {
+            if file.state != filesWithState {
+                filesCopy.removeValueForKey(identifier)
+            }
+        }
+        files = filesCopy
+    }
+}
+
+
+extension CheapjackManager: NSURLSessionDownloadDelegate {
+    
+    public func URLSession(session: NSURLSession, downloadTask: NSURLSessionDownloadTask, didFinishDownloadingToURL location: NSURL) {
+        if let file = files[downloadTask.taskDescription!] {
+            file.setState(.Finished)
+            delegate?.cheapjackManager(self, didFinishDownloading: session, downloadTask: downloadTask, url: location, forFile: file)
+            if let didFinishDownloadingBlock = didFinishDownloadingBlock {
+                didFinishDownloadingBlock(session: session, downloadTask: downloadTask, url: location, file: file)
+            }
+        }
+    }
+    
+    public func URLSession(session: NSURLSession, downloadTask: NSURLSessionDownloadTask, didWriteData bytesWritten: Int64, totalBytesWritten: Int64, totalBytesExpectedToWrite: Int64) {
+        if let file = files[downloadTask.taskDescription!] {
+            if file.state != CheapjackFile.State.Downloading {
+                file.setState(.Downloading)
+            }
+            
+            file.setTotalBytesWritten(totalBytesWritten)
+            file.setTotalBytesExpectedToWrite(totalBytesExpectedToWrite)
+        }
+    }
+    
+    public func URLSession(session: NSURLSession, downloadTask: NSURLSessionDownloadTask, didResumeAtOffset fileOffset: Int64, expectedTotalBytes: Int64) {
+        print("didResumeAtOffset")
+    }
+    
+}
+
+
+extension CheapjackManager: NSURLSessionDelegate {
+    
+    public func URLSession(session: NSURLSession, task: NSURLSessionTask, didCompleteWithError error: NSError?) {
+        print("******** COMPLETE!!!!")
+        if let error = error {
+            print(error)
+            delegate?.cheapjackManager(self, didReceiveError: error)
+        }
+    }
+    
+    public func URLSession(session: NSURLSession, didBecomeInvalidWithError error: NSError?) {
+        if let error = error {
+            print(error)
+            delegate?.cheapjackManager(self, didReceiveError: error)
+        }
+    }
+    
+    public func URLSessionDidFinishEventsForBackgroundURLSession(session: NSURLSession) {
+        session.getTasksWithCompletionHandler { dataTasks, uploadTasks, downloadTasks in
+            if downloadTasks.count == 0 {
+                
+            }
+        }
+    }
+    
+}
